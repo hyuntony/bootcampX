85348616096f688be62d01480cd12be3554e17d3
hyuntony <ankim90@gmail.com>
2021-06-22 19:47:42 -0400

    completed 4_queries assignment

---
+++ 4_queries/1_total_requests.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.id) as total_assistances, name
+FROM teachers JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY name
